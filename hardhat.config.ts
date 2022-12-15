88e0687dc532f87d8a0214c0b74ffd0aea6593f8
Paul Brower <paul@team.casa>
2022-12-15 14:20:23 -0700

    include dotenv, deploying to sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv"
+dotenv.config()
+
+const { HH_API_URL, HH_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: HH_API_URL,
+      accounts: [HH_PRIVATE_KEY as string],
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,14 +5,16 @@ async function main() {
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const lockedAmount = ethers.utils.parseEther("0.001");
+  const signers = await ethers.getSigners()
+  console.log(`signers: ${signers.map(s => s.address).join(",")}`)
 
   const Lock = await ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
   await lock.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Lock with ${ethers.utils.formatEther(lockedAmount)} WEI and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
