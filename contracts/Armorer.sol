3aeb769acc0e1d3ac31842afbee770adf34cd63b
Paul Brower <paul@team.casa>
2023-01-23 15:03:03 -0700

    test for deploying df1

--- contracts/Armorer.sol
+++ contracts/Armorer.sol
@@ -9,4 +9,10 @@ contract Armorer {
     function getA() public view returns (uint256) {
         return a;
     }
+
+    function yoink(address payable _addr) public payable {
+        (bool success, bytes memory data) = _addr.call{value: msg.value, gas: gasleft()}(
+            abi.encodeWithSignature("withdraw()", "call foo", 123)
+        );
+    }
 }
--- test/Armorer.ts
+++ test/Armorer.ts
@@ -1,17 +1,36 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
+const ONE_GWEI = 1_000_000_000;
 
 describe("Armorer", function () {
 
+    async function deployArmorer() {
+        const Armorer = await ethers.getContractFactory("Armorer");
+        const armorer = await Armorer.deploy();
+        return armorer
+    }
     describe("deployment", function () {
         it("should deploy the contract", async function () {
-            const Armorer = await ethers.getContractFactory("Armorer");
-            const armorer = await Armorer.deploy();
-            const a  = await armorer.getA()
+            const armorer = await loadFixture(deployArmorer)
+            expect(await armorer.getA()).to.equal(1);
+        });
+    })
 
-            expect(a).to.equal(1);
+    describe("contract interaction", function () {
+        it("should be able to withdraw from v1 DF", async function () {
+            const armorer = await loadFixture(deployArmorer)
+
+            // deploy df1
+            const DarkForest1 = await ethers.getContractFactory("DarkForestV1");
+            const df1 = await DarkForest1.deploy({ value: ONE_GWEI })
+            const df1Balance = await ethers.getDefaultProvider().getBalance(df1.address);
+            expect(df1Balance).to.equal(ONE_GWEI);
+
         });
 
     })
+
+
+
 })
