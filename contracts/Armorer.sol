e7d5589a1abe3a30f7cd12b2ece521713a97cf2b
Paul Brower <paul@team.casa>
2023-01-24 13:29:59 -0700

    working with call

--- contracts/Armorer.sol
+++ contracts/Armorer.sol
@@ -1,8 +1,4 @@
-//interface DF {
-//    function withdraw() external payable;
-//}
-
-import "./DarkForestV1.sol";
+// SPDX-License-Identifier: MIT
 
 contract Armorer {
 
@@ -10,10 +6,12 @@ contract Armorer {
     }
 
     function yoink(address payable _addr) public payable {
-        (bool success) = _addr.call{value: msg.value, gas: gasleft()}(
+        uint256 startBalance = address(this).balance;
+        (bool success, bytes memory data) = _addr.call{value: msg.value, gas: gasleft()}(
             abi.encodeWithSignature("withdraw()")
         );
         require(success, "yoink was not successful");
+        require(address(this).balance > startBalance, "balance was not increased");
     }
 
     // needed to add to the balance
