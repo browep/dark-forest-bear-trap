ec1b0dea69b18c8ee6ae8fcb8979e44edec6f4fc
Paul Brower <paul@team.casa>
2023-01-24 12:09:31 -0700

    straight call working, but not contract to contract

--- contracts/Armorer.sol
+++ contracts/Armorer.sol
@@ -1,20 +1,18 @@
+interface DF {
+    function withdraw() external;
+}
+
 contract Armorer {
 
-    uint256 public a;
-    address public owner;
-
     constructor() {
-        a = 1;
-        owner = msg.sender;
-    }
-
-    function getA() public view returns (uint256) {
-        return a;
     }
 
     function yoink(address payable _addr) public payable {
-        (bool success, bytes memory data) = _addr.call{value: msg.value, gas: gasleft()}(
-            abi.encodeWithSignature("withdraw()", "call foo", 123)
-        );
+//        (bool success, bytes memory data) = _addr.call{value: msg.value, gas: gasleft()}(
+//            abi.encodeWithSignature("withdraw()")
+//        );
+        DF(_addr).withdraw();
+
+//        require(success, "yoink was not successful");
     }
 }
--- test/Armorer.ts
+++ test/Armorer.ts
@@ -9,34 +9,57 @@ describe("Armorer", function () {
         const Armorer = await ethers.getContractFactory("Armorer");
         return await Armorer.deploy();
     }
-    describe("deployment", function () {
-        it("should deploy the contract", async function () {
-            const armorer = await loadFixture(deployArmorer)
-            expect(await armorer.getA()).to.equal(1);
-            const [owner] = await ethers.getSigners();
-            expect(await armorer.owner()).to.equal(owner.address);
-            console.log(`network name: ${network.name}`);
-            console.log(`owner: ${owner.address}`)
-            console.log(`get Balance: ${await ethers.provider.getBalance(owner.address)}`)
-            console.log(`default network: ${ethers.getDefaultProvider().network.name}`)
 
+    async function deployArmorerAndDF1() {
+        const Armorer = await ethers.getContractFactory("Armorer");
+        const armorer = await Armorer.deploy();
+
+        const DarkForest1 = await ethers.getContractFactory("DarkForestV1");
+        const df1 = await DarkForest1.deploy({value: ONE_GWEI})
+        let df1Balance = await ethers.provider.getBalance(df1.address);
+        expect(df1Balance).to.equal(ethers.BigNumber.from(ONE_GWEI));
+
+        return {armorer, df1}
+    }
+
+    describe("deployment", function () {
+        it("should deploy the armorer contract", async function () {
+            const armorer = await loadFixture(deployArmorer)
         });
     })
 
     describe("contract interaction", function () {
-        it("should be able to withdraw from v1 DF", async function () {
-            const armorer = await loadFixture(deployArmorer)
 
+        it("should be able to withdraw straight from v1 DF", async function () {
             // deploy df1
-            const DarkForest1 = await ethers.getContractFactory("DarkForestV1");
-            const df1 = await DarkForest1.deploy({value: ONE_GWEI})
-            const df1Balance = await ethers.provider.getBalance(df1.address);
+            const {armorer, df1} = await loadFixture(deployArmorerAndDF1);
+            let df1Balance = await ethers.provider.getBalance(df1.address);
             expect(df1Balance).to.equal(ethers.BigNumber.from(ONE_GWEI));
+
+            // withdraw straight
+            await df1.withdraw()
+
+            // check balance
+            df1Balance = await ethers.provider.getBalance(df1.address);
+            expect(df1Balance).to.equal(ethers.BigNumber.from(0));
+
+        })
+
+        it("should be able to withdraw from v1 DF", async function () {
+            const {armorer, df1} = await loadFixture(deployArmorerAndDF1);
+
+            let df1Balance = await ethers.provider.getBalance(df1.address);
+            expect(df1Balance).to.equal(ethers.BigNumber.from(ONE_GWEI));
+
+            // try to get funds from df1
+            const yoinkTx = await armorer.yoink(df1.address)
+
+            // df1 should have no funds now
+            df1Balance = await ethers.provider.getBalance(df1.address);
+            expect(df1Balance).to.equal(ethers.BigNumber.from(0));
 
         });
 
     })
-
-
 
 })
